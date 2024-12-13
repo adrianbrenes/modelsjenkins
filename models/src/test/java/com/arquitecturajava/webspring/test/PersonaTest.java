e0122e4945651540e1e78a881fda445a3b588ca5
sistemas.madrid <sistemas.madrid@host.docker.internal>
2024-12-13 16:08:51 +0100

    Modificación

--- models/src/test/java/com/arquitecturajava/webspring/test/PersonaTest.java
+++ models/src/test/java/com/arquitecturajava/webspring/test/PersonaTest.java
@@ -9,10 +9,30 @@ import com.arquitecturajava.webspring.models.Persona;
 class PersonaTest {
 
 	@Test
-	void MayorEdadTest() {
+	void MayorEdadTest1() {
 		Persona p = new Persona("Pepe", 20);
 		boolean esMayor = p.esMayorEdad();
 		assertTrue(esMayor);
 	}
 
+	@Test
+	void MayorEdadTest2() {
+		Persona p = new Persona("Pepe", 20);
+		boolean esMayor = p.esMayorEdad();
+		assertTrue(esMayor);
+	}
+	
+	@Test
+	void MayorEdadTest3() {
+		Persona p = new Persona("Pepe", 20);
+		boolean esMayor = p.esMayorEdad();
+		assertTrue(esMayor);
+	}
+	
+	@Test
+	void MayorEdadTest4() {
+		Persona p = new Persona("Pepe", 10);
+		boolean esMayor = p.esMayorEdad();
+		assertTrue(esMayor);
+	}
 }
